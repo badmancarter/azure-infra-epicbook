0feb618c7ddced9b74c151e74d099d5662cd12f7
Olabode <159830183+badmancarter@users.noreply.github.com>
2026-04-27 23:30:32 +0100

    Change Terraform state key for Azure backend

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     resource_group_name  = "epicbook-tfstate-rg"
     storage_account_name = "epicbooktf26042793"
     container_name       = "epicbooktfstate"
-    key                  = "aws-infra-epicbook.tfstate"
+    key                  = "azure-infra-epicbook.tfstate"
     use_azuread_auth     = true
   }
+}
