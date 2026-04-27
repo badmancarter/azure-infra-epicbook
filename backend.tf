e1c27692725ffc0878cd669ca52ffdfa4c8079df
Olabode <159830183+badmancarter@users.noreply.github.com>
2026-04-27 23:25:42 +0100

    Update Azure backend configuration for Terraform

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "epicbook-tfstate-rg"
-    storage_account_name = "epicbooktfstate3657"
-    container_name       = "epibooktfstate"
-    key                  = "azure-infra-epicbook.tfstate"
+    storage_account_name = "epicbooktf26042793"
+    container_name       = "epicbooktfstate"
+    key                  = "aws-infra-epicbook.tfstate"
     use_azuread_auth     = true
   }
-}
